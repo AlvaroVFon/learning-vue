423cbab1c9affb1e4bf03a89f483160f3a735b6e
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-05-16 12:59:03 +0200

    GridWrapper added

---
+++ src/components/GridWrapper.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 4xl:grid-cols-6 gap-y-10 place-items-center p-6"
+  >
+    <slot />
+  </div>
+</template>
--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,18 +1,16 @@
 <template>
-  <h1>Hello from Courses Page</h1>
-  <div
-    class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 4xl:grid-cols-6 gap-y-10 place-items-center p-6"
-  >
+  <GridWrapper>
     <template
       v-for="course in courses"
       :key="course.id"
     >
       <CourseCard :course="course" />
     </template>
-  </div>
+  </GridWrapper>
 </template>
 <script setup>
   import CourseCard from '../components/CourseCard.vue'
+  import GridWrapper from '../components/GridWrapper.vue'
   import { ref, onMounted } from 'vue'
   import api from '@/utils/WebinarApi'
 
