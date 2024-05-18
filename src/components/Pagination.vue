8c796afb44ee740eddc7ec171ed0aec2a7e2e621
Álvaro Villamarín <alvaro_90_xti@hotmail.com>
2024-05-18 15:58:59 +0200

    iconos pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="text-accent w-full flex justify-center items-center">
+  <div class="text-accent w-full flex justify-center items-center gap-3">
+    <button>
+      <FirstPageIcon color="#42b883" />
+    </button>
     <button
       @click="decrement"
-      class="border"
+      class="bg-accent text-primary font-semibold hover:bg-accentDarker p-2 rounded-md w-24 duration-300"
     >
       Previous
     </button>
-    {{ page }}
-    <button @click="increment">Next</button>
+
+    <button
+      @click="increment"
+      class="bg-accent text-primary font-semibold hover:bg-accentDarker p-2 rounded-md w-24 duration-300"
+    >
+      Next
+    </button>
+    <button>
+      <LastPageIcon color="#42b883" />
+    </button>
   </div>
 </template>
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import FirstPageIcon from '@/components/icons/FirstPageIcon.vue';
+import LastPageIcon from '@/components/icons/LastPageIcon.vue';
 const router = useRouter();
 
 const page = ref(router.currentRoute.value.query.page || 1);
---
+++ src/components/icons/FirstPageIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24"
+  >
+    <path
+      :fill="color"
+      d="M7 18q-.425 0-.712-.288T6 17V7q0-.425.288-.712T7 6t.713.288T8 7v10q0 .425-.288.713T7 18m6.8-6l3.9 3.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275l-4.6-4.6q-.15-.15-.212-.325T11.425 12t.063-.375t.212-.325l4.6-4.6q.275-.275.7-.275t.7.275t.275.7t-.275.7z"
+    />
+  </svg>
+</template>
+<script setup>
+defineProps({
+  color: {
+    type: String,
+    default: '#000000',
+  },
+  size: {
+    type: String,
+    default: '25',
+  },
+});
+</script>
