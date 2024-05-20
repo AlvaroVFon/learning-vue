a035aad5041ad69196638d7e26ac436c4c5c568a
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-05-20 08:14:24 +0200

    CourseCardSkeleton finished

---
+++ src/components/CourseCardSkeleton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div
+    class="w-[285px] h-[414px] p-8 bg-slate-300 rounded-xl flex flex-col items-center gap-3 animate-pulse"
+  >
+    <div class="title w-[250px] h-6 bg-slate-400 rounded-lg"></div>
+    <div class="w-[285px] flex justify-end p-5">
+      <div class="category w-[80px] h-8 bg-slate-400 rounded-lg"></div>
+    </div>
+    <div class="img w-[250px] h-[160px] bg-slate-400 rounded-lg"></div>
+    <div class="flex justify-evenly w-[284px] mt-6">
+      <div class="w-[30px] h-8 bg-slate-400 rounded-lg"></div>
+      <div class="w-[30px] h-8 bg-slate-400 rounded-lg"></div>
+      <div class="w-[30px] h-8 bg-slate-400 rounded-lg"></div>
+    </div>
+  </div>
+</template>
+<script setup></script>
--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -5,34 +5,40 @@
       v-for="course in courses"
       :key="course.id"
     >
-      <CourseCard :course="course" />
+      <Suspense>
+        <CourseCard :course="course" />
+        <template #fallback>
+          <CourseCardSkeleton />
+        </template>
+      </Suspense>
     </template>
   </GridWrapper>
   <Pagination :totalPages="totalPages" />
 </template>
 <script setup>
-import CourseCard from '@/components/CourseCard.vue';
-import GridWrapper from '@/components/GridWrapper.vue';
-import Pagination from '@/components/Pagination.vue';
-import { ref, onMounted, watch } from 'vue';
-import { useRoute } from 'vue-router';
-import api from '@/utils/WebinarApi';
+  import CourseCard from '@/components/CourseCard.vue'
+  import GridWrapper from '@/components/GridWrapper.vue'
+  import CourseCardSkeleton from '@/components/CourseCardSkeleton.vue'
+  import Pagination from '@/components/Pagination.vue'
+  import { ref, onMounted, watch } from 'vue'
+  import { useRoute } from 'vue-router'
+  import api from '@/utils/WebinarApi'
 
-const route = useRoute();
-const courses = ref([]);
-const error = ref(null);
-const isLoading = ref(false);
-const totalPages = ref(0);
-const getCourses = async (url) => {
-  isLoading.value = true;
-  const response = await api.getCourses(url);
-  courses.value = response.results;
-  totalPages.value = response.totalPages;
-};
-onMounted(() => {
-  getCourses(route.fullPath);
-});
-watch(route, () => {
-  getCourses(route.fullPath);
-});
+  const route = useRoute()
+  const courses = ref([])
+  const error = ref(null)
+  const isLoading = ref(false)
+  const totalPages = ref(0)
+  const getCourses = async (url) => {
+    isLoading.value = true
+    const response = await api.getCourses(url)
+    courses.value = response.results
+    totalPages.value = response.totalPages
+  }
+  onMounted(() => {
+    getCourses(route.fullPath)
+  })
+  watch(route, () => {
+    getCourses(route.fullPath)
+  })
 </script>
